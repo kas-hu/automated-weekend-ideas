6a56b37b6379d320d2edb4c78751094f4312b92d
Mohammed Kasim <70877778+logicdictates@users.noreply.github.com>
2022-10-24 21:21:08 +0100

    first
    add some things to do

---
+++ main.py
@@ -0,0 +1,4 @@
+from random import choice 
+
+# list of things to do 
+things = ['anime', 'movie with friends', 'cinema', 'travel to london', 'visit brother', 'visit sister', 'restaurant', 'swimming']
