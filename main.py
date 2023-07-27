aaab26bbb1fa1f07e9b4fc12a643e3d8bed105dc
Mohammed Kasim <70877778+logicdictates@users.noreply.github.com>
2023-07-27 22:15:55 +0100

    add random function

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from random import choice 
+import random
 
 # list of things to do 
-things = ['anime', 'movie with friends', 'cinema', 'travel to london', 'visit brother', 'visit sister', 'restaurant', 'swimming']
+things = ['anime', 'movie with friends', 'cinema', 'travel to london', 'visit brother', 'visit sister', 'restaurant', 'swimming', 'take mum out']
+
+x = random.choice(things)
+print(x)
